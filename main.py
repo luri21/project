7ef3e4e90971ba155dbdbc59415044b945c0ce57
luri21 <illiarepchanskiy@gmail.com>
2023-04-29 09:36:24 +0300

    привітання для вчителя

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print ('добрий вечір')
+class Student:
+    def __init__(self, name, surfname, age ):
+        self.name = name
+        self.surfname = surfname
+        self.age = age
+
+
+s1 = Student("Oleg","Havron",25)
+print(s1.name)
+print(s1.surfname)
+print(s1.age)
