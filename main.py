f817f8586e0000af861dfa70fdf9866206efae05
luri21 <illiarepchanskiy@gmail.com>
2023-05-06 11:25:58 +0300

    привітання для вчителя

--- main.py
+++ main.py
@@ -1,11 +1,38 @@
-
-
-class Ilur:
-    def init(self, name, list, ):
+class Person:
+    def __init__(self, name, age, gender):
         self.name = name
-        self.list = list
-    def average(self):
-        print(f"Averege: {sum(self.list) / len(self.list)}")
+        self.age = age
+        self.gender = gender
 
-s1 = Ilur("ilur", [9, 12, 10, 8 ,5 , 10 , 4 , 7])
-s1.average()
+
+class Student(Person):
+    def __init__(self, name, age, gender, student_id):
+        super().__init__(name, age, gender)
+        self.student_id = student_id
+
+    def say_hello(self):
+        print(f"Hello, my name is {self.name} and my student ID is {self.student_id}")
+
+
+class Teacher(Person):
+    def __init__(self, name, age, gender, subject):
+        super().__init__(name, age, gender)
+        self.subject = subject
+
+    def say_hello(self):
+        print(f"Hello, my name is {self.name} and I teach {self.subject}")
+
+
+class Engineer(Person):
+    def __init__(self, name, age, gender, company):
+        super().__init__(name, age, gender)
+        self.company = company
+
+    def say_hello(self):
+        print(f"Hello, my name is {self.name} and I work at {self.company}")
+student = Student("Дем'ян", 12, "male", "S12345")
+teacher = Teacher("Ігор", 24, "female", "Python")
+engineer = Engineer("Peter", 40, "male", "Google")
+student.say_hello()
+teacher.say_hello()
+engineer.say_hello()
